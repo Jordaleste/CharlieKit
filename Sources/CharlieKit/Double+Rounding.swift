4e174d199ed69ab3e60cf16cc904b1d5a1f7e894
Charlie Eison <charlie@esnenterprises.com>
2020-03-28 21:39:42 -0400

    Added Float Rounding

---
+++ Sources/CharlieKit/Double+Rounding.swift
@@ -0,0 +1,28 @@
+//
+//  Double+Rounding.swift
+//  
+//
+//  Created by Charles Eison on 3/28/20.
+//
+
+import Foundation
+
+public extension Double {
+    
+    ///Rounds to a number of decimal places
+    mutating func round(toDecimalPlaces decimalPlaces: Int) {
+        let multiplier = pow(10, Double(decimalPlaces))
+        
+        self *= multiplier
+        self += 0.5
+        self = Double(Int(self))
+        self /= multiplier
+    }
+    
+    ///Returns the number, rounded to a number of decimal places
+    func rounded(toDecimalPlaces decimalPlaces: Int) -> Double {
+        var numberToRound = self
+        numberToRound.round(toDecimalPlaces: decimalPlaces)
+        return numberToRound
+    }
+}
--- Sources/CharlieKit/Float+Rounding.swift
+++ Sources/CharlieKit/Float+Rounding.swift
@@ -1,26 +1,26 @@
 //
-//  File.swift
-//  
+//  Float+Rounding.swift
+//
 //
 //  Created by Charles Eison on 3/28/20.
 //
 
 import Foundation
 
-public extension Double {
+public extension Float {
     
     ///Rounds to a number of decimal places
     mutating func round(toDecimalPlaces decimalPlaces: Int) {
-        let multiplier = pow(10, Double(decimalPlaces))
+        let multiplier = pow(10, Float(decimalPlaces))
         
         self *= multiplier
         self += 0.5
-        self = Double(Int(self))
+        self = Float(Int(self))
         self /= multiplier
     }
     
     ///Returns the number, rounded to a number of decimal places
-    func rounded(toDecimalPlaces decimalPlaces: Int) -> Double {
+    func rounded(toDecimalPlaces decimalPlaces: Int) -> Float {
         var numberToRound = self
         numberToRound.round(toDecimalPlaces: decimalPlaces)
         return numberToRound
